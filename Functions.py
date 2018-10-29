30711165870f3209149a563f29f3f8bd981fc595
user <braunsoskars@gmail.com>
2018-10-29 11:25:51 +0200

    RTR105

---
+++ Functions.py
@@ -0,0 +1,15 @@
+def thing():
+    print('Hello')
+    print('Fun')
+thing()
+print('ZIP')
+thing()
+
+def greet(lang):
+    if lang =='es':
+        print('Hola')
+    elif lang =='fr':
+        print('Bonjour')
+    else:
+        print('Hello')
+
