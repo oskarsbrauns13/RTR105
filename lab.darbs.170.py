696e191188edf5e0bfabfd2de6ff2323be2e3a32
oskarsbrauns13 <43132609+oskarsbrauns13@users.noreply.github.com>
2018-11-26 13:51:01 +0200

    Add files via upload

---
+++ lab.darbs.170.py
@@ -0,0 +1,22 @@
+from math import sin, fabs
+from time import sleep
+def f(x):
+    return sin(x)
+a = 1.1
+b = 3.2
+funa = f(a)
+funb = f(b)
+if ( funa * funb > 0.0 ):
+    print("Dotajaa intervaalaa [%s, %s] saknju nav"%(a,b))
+    sleep(1); exit()
+else:
+    print("Dotajaa intervaalaa sakne (s) ir!")
+deltax = 0.01
+
+while ( fabs(b-a) > deltax ):
+    x = (a+b)/2; funx = f(x)
+    if ( funa*funx < 0. ):
+        b = x
+    else:
+        a = x
+print ("Sakne ir: ", x)
